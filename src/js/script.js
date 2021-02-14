d148747d1b4043ee137072793279caa1d1c98e14
MarkelovArtemKa <71997812+MarkelovArtemKa@users.noreply.github.com>
2021-02-13 20:37:09 -0600

    Add files via upload

--- src/js/script.js
+++ src/js/script.js
@@ -75,4 +75,9 @@ $('.study_grow').each(function(i){
       });
       return false;
   });
+  $("a[href^='#']").click(function(){
+    const _href = $(this).attr("href");
+    $("html, body").animate({scrollTop: $(_href).offset().top+"px"});
+    return false;
+    });
 });
